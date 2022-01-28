22b224ea88f3e035a0d3f458027aa7329d1ec709
MrMakako <diegofdiaz1717@gmail.com>
2022-01-28 11:45:01 -0800

    Creando primeras clases

---
+++ src/Animal.java
@@ -0,0 +1,30 @@
+public class Animal {
+
+
+    private String NombreCientifico;
+
+
+
+    private String NombreComun,Descripcion,Habitat,Alimentacion,Geografia;
+
+    public Animal(){
+
+    }
+
+
+    public Animal(String NombreCientifico,String NombreComun, String Descripcion,String Habitat,String Alimentacion,String Geografia) {
+
+        this.NombreCientifico=NombreCientifico;
+        this.NombreComun=NombreComun;
+        this.Descripcion=Descripcion;
+        this.Habitat=Habitat;
+        this.Alimentacion=Alimentacion;
+        this.Geografia=Geografia;
+
+
+    }
+
+
+
+
+}
---
+++ src/Registro.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class Registro extends Animal {
+    ArrayList<Animal> Animales= new ArrayList<Animal>();
+
+
+    public Registro(){
+        super();
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
